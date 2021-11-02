c7a4959e75c407dcd187af6b8dc7029670ccdef8
popcom <mojabi.mohsen@gmail.com>
2021-11-02 19:09:43 +0330

    Add project files.

---
+++ ReddisWebApi/Controllers/ProductController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using ReddisWebApi.Data;
+using ReddisWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReddisWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IDistributedCache _cache;
+
+        public ProductController(ApplicationDbContext context, IDistributedCache cache)
+        {
+            _context = context;
+            _cache = cache;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            //var products = await _context.Products.ToListAsync();
+
+            //return Ok(products);
+
+
+
+            var cacheKey = "GET_ALL_PRODUCTS";
+            List<Product> products = new List<Product>();
+
+            // Get data from cache
+            var cachedData = await _cache.GetAsync(cacheKey);
+            if (cachedData != null)
+            {
+                // If data found in cache, encode and deserialize cached data
+                var cachedDataString = Encoding.UTF8.GetString(cachedData);
+                products = JsonConvert.DeserializeObject<List<Product>>(cachedDataString);
+            }
+            else
+            {
+                // If not found, then fetch data from database
+                products = await _context.Products.ToListAsync();
+
+                // serialize data
+                var cachedDataString = JsonConvert.SerializeObject(products);
+                var newDataToCache = Encoding.UTF8.GetBytes(cachedDataString);
+
+                // set cache options 
+                var options = new DistributedCacheEntryOptions()
+                    .SetAbsoluteExpiration(DateTime.Now.AddMinutes(2))
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(1));
+
+                // Add data in cache
+                await _cache.SetAsync(cacheKey, newDataToCache, options);
+            }
+
+            return Ok(products);
+        }
+
+    }
+}
---
+++ ReddisWebApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ReddisWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReddisWebApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            Product[] products = new Product[10000];
+            for (int i = 1; i <= 10000; i++)
+            {
+                products[i - 1] = new Product() { Id = i, Name = "Product #" + i, Price = i * 1000 };
+            }
+            modelBuilder.Entity<Product>().HasData(products);
+        }
+    }
+}
